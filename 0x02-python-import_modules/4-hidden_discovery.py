ff7daeee32ffad8d8942e1303581628e967932f2
SmileeCj <chejaiismail@gmail.com>
2024-09-16 15:26:06 +0100

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import hidden_4 
+    import hidden_4
 
     ls = dir(hidden_4)
-    
+
     for x in ls:
         if x[:2] == '__':
             continue
         print(f"{x}")
-
