6bfdd4b5d477feea3542d1de5eedd6d1613721be
SmileeCj <chejaiismail@gmail.com>
2024-09-16 14:05:22 +0100

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     if n == 1:
         print("0 arguments.")
     elif n == 2:
-        print("{} argument:".format(n - 1))
+        print("1 argument:")
         print("{}: {}".format(1, sys.argv[1]))
     else:
         print("{} arguments:".format(n - 1))
