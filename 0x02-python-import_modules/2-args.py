4d17135502fadcd86d935fc13acf3e6cf59162d1
SmileeCj <chejaiismail@gmail.com>
2024-09-16 14:12:16 +0100

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
         print("1 argument:")
         print("{}: {}".format(1, sys.argv[1]))
     else:
-        print("{} arguments:".format(n - 1))
-    for x in range(1, n):
-        print("{}: {}".format(x, sys.argv[x]))
+        print("{} arguments:".format(n))
+        for x in range(1, n + 1):
+            print("{}: {}".format(x, sys.argv[x]))
